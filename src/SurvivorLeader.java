7f4636e9c80b7871488c7de51540a2cd3d1b6811
ms-lockwood <tori_lockwood@milton.edu>
2025-01-21 10:34:56 -0500

    Initial commit

---
+++ src/SurvivorLeader.java
@@ -0,0 +1,3 @@
+public class SurvivorLeader {
+    private String transmission = "q2xf1t8r5w3t tm9c5i8l900d5n7heto 3g5t6hgw44y5e7bhty605rh8t5 85ml95ak2k5r7qw5ksp295e5t7p45 o5i3n 6z5n205 9soe5o8hhg5lygtt5dq7th 5 gf34s9o 35j7j5u45k99hg5kyq5aqc5sjg5jkpl20z5a629sle5r20sk5vkkdjhgf 5i9c5 gh73h qxp25vi8t5i3f2v1q 9x7z6t5l8r9j3o4v5l3l1w 9y5a4d6f7x2c9l5g h8s45ek6";
+}
